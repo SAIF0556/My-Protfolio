30fe1d2d54390129296244e1ffb4b37c72508935
SM SAIF <smsaif767@gmail.com>
2024-05-06 18:26:26 +0530

    Contact Added

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './portfolio.scss'
+import { motion, useScroll, useSpring, useTransform } from 'framer-motion'
 
 const items = [
   {
     id: 1,
     title: 'React Meet Up',
-    img: './pexels-fauxels-3183150.jpg',
+    img:
+      'https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     desc:
       ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod minus, quibusdam molestias odio provident amet aut maiores saepe sunt distinctio?',
   },
@@ -32,8 +34,60 @@ const items = [
   },
 ]
 
+const Single = ({ item }) => {
+  const ref = useRef()
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  })
+
+  const y = useTransform(scrollYProgress, [0, 1], [-35, 35])
+
+  return (
+    <section>
+      <div className="container">
+        <div className="wrapper">
+          <motion.div className="imgContainer" ref={ref}>
+            <img src={item.img} alt="img" />
+          </motion.div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>See Project</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
 function Portfolio() {
-  return <div className="portfolio">Portfolio</div>
+  const ref = useRef()
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['end end', 'start start'],
+  })
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  })
+
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Featured Work</h1>
+        <motion.div
+          style={{ scaleX: scaleX }}
+          className="progressBar"
+        ></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  )
 }
 
 export default Portfolio
